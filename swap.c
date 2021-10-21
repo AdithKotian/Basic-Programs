86fe00ea3c8be831efb43d4ff61a0b8d93be953e
AdithKotian <92904121+AdithKotian@users.noreply.github.com>
2021-10-21 16:15:21 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+#include<iomanip.h>
+void main()
+{
+    clrscr();
+int a,b;
+cout<<"Enter the 2 numbers"<<endl;
+cin>>a>>b;
+a=a+b;
+b=a-b;
+a=a-b;
+cout<<"output is"<<a<<"\t" b<<endl;
+getch();
+}
+
+
