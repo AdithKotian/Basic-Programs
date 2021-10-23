b8b97cc04d738b4e2ab22d92f7bcf196e571b057
AdithKotian <92904121+AdithKotian@users.noreply.github.com>
2021-10-23 19:24:38 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int a,b,sum=0;
+    cout<<"Enter the 2 numbers "<<endl;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"The sum of two numbers is "<<sum<<endl;
+    
+}
